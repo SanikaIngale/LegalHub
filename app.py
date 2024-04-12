70ab361254fadb6f8f34ce1cff217023b9a70ace
ISHWARISD <ishwaridehadray1501@gmail.com>
2024-04-12 23:48:22 +0530

    LAST COMMIT OF LAWLAWLAW

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, redirect, render_template, request, jsonify, send_from_directory, url_for
+from flask import Flask, redirect, render_template, request, jsonify, session, send_from_directory, url_for
 import google.generativeai as genai
 from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'  # Set a secret key for session management
 
 genai.configure(api_key="AIzaSyBd385XupzEu4oYXmWC2aZxBGj5pja4kHI")
 model = genai.GenerativeModel("gemini-pro")
@@ -16,15 +17,18 @@ users_collection = db['users']  # Change this to your collection name
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    logged_in = session.get('logged_in', False)
+    return render_template('index.html', logged_in=logged_in)
 
 @app.route('/page2')
 def page2():
-    return render_template('page2.html')
+    logged_in = session.get('logged_in', False)
+    return render_template('page2.html', logged_in=logged_in)
 
 @app.route('/templatess')
 def templatess():
-    return render_template('templatess.html')
+    logged_in = session.get('logged_in', False)
+    return render_template('templatess.html', logged_in=logged_in)
 
 @app.route('/pdfs/<path:filename>')
 def pdfs(filename):
@@ -48,7 +52,6 @@ def signup():
     #     users_collection.insert_one({'fullname': fullname, 'email': email, 'password': password})
     #     return redirect(url_for('login'))  # Redirect to the login page after signup
     return render_template('signup.html')
-from flask import request, redirect, url_for, render_template
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -59,28 +62,38 @@ def login():
         user = users_collection.find_one({'fullname': fullname, 'password': password})
         if user:
             # Successful login
-            return render_template('index.html')
+            session['logged_in'] = True
+            return render_template('index.html', logged_in=True)
         else:
             # Login failed, render the login page again with an alert message
             return render_template('login.html', alert_message='Invalid username or password')
     return render_template('login.html', alert_message=None)
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)  # Remove the 'logged_in' session variable
+    return redirect(url_for('index'))  # Redirect to the index page after logout
+
 
 @app.route('/legalCases')
 def legalCases():
-    return render_template('legalCases.html')
+    logged_in = session.get('logged_in', False)
+    return render_template('legalCases.html', logged_in=logged_in)
 
 @app.route('/lawyersPage')
 def lawyersPage():
-    return render_template('lawyersPage.html')
+    logged_in = session.get('logged_in', False)
+    return render_template('lawyersPage.html', logged_in=logged_in)
 
 @app.route('/lawinfoo')
 def lawinfoo():
-    return render_template('lawinfoo.html')
+    logged_in = session.get('logged_in', False)
+    return render_template('lawinfoo.html', logged_in=logged_in)
 
 @app.route('/applicationForm')
 def applicationForm():
-    return render_template('applicationForm.html')
+    logged_in = session.get('logged_in', False)
+    return render_template('applicationForm.html', logged_in=logged_in)
 
 @app.route('/process_input', methods=['POST'])
 def process_input():
@@ -98,4 +111,4 @@ def get_query(query):
     return response.text
 
 if __name__ == '__main__':
-    app.run(debug=True,port=3000)
+    app.run(debug=True, port=3000)
